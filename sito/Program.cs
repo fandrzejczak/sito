7a11321d942042271471a4d91e6ff53436ea27ce
fandrzejczak <106021371+fandrzejczak@users.noreply.github.com>
2023-03-01 05:07:10 +0100

    Dodaj pliki projektów.

---
+++ sito/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace sito
+{
+    class Program
+    {
+        public static bool czyPierwsza(int a)
+        {
+            if (a < 2)
+                return false;
+            for (int i = 2; i < a; i++)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj ciąg liczb oddzielając je przecinkiem.");
+            string s1 = Console.ReadLine();
+            string[] s2 = s1.Split(',');
+
+            foreach (var s3 in s2)
+            {
+                int a = System.Int32.Parse(s3);
+                bool result = czyPierwsza(a);
+                if (result)
+                {
+                    Console.WriteLine(a);
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
